e046af13d096440bfed591b527acbf36afa326b0
MUVivekanand <22z275@psgtech.ac.in>
2025-01-07 13:41:51 +0530

    added frontend

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter and Routes
+import './styles/index.css';
+import App from './App';
+import Reports from './Reports';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter> {/* Wrap the app with BrowserRouter */}
+      <Routes> {/* Define the routes */}
+        <Route path="/" element={<Reports />} />
+        <Route path="/app" element={<App />} /> 
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
